e6d174421ec5195c89cb0f91fec5e50107f4372a
LD <LDinho@users.noreply.github.com>
2019-05-25 17:33:24 -0400

    add post new zoo endpoint
    add favicon.ico to prevent chrome browser 404 favicon not found error

--- api/routes/zoos/zooRoutes.js
+++ api/routes/zoos/zooRoutes.js
@@ -5,9 +5,10 @@ const router = express.Router();
 const db = require('../../../data/dbConfig');
 
 // GET
+
 router.get('/', async (req, res) => {
   try {
-    const zoos = await db('zoos');
+    const zoos = await db('zoos'); // SQL - select * from zoos
 
     if (zoos.length) {
       res.status(200).json(zoos)
@@ -22,5 +23,55 @@ router.get('/', async (req, res) => {
   }
 
 });
+
+// GET by id
+
+router.get('/:id', async (req, res) => {
+
+
+});
+
+//POST
+
+router.post('/', async (req, res) => {
+  const newZoo = req.body;
+
+  try {
+    const { name } = newZoo;
+
+    if (!name) {
+      return res.status(400).json({error: 'name missing'});
+    } else {
+
+      const zoo = await db('zoos')
+        .insert(newZoo, 'id');
+
+      res.status(201).json(zoo);
+    }
+
+  }
+  catch (err) {
+    return res.status(500)
+      .json({
+        err,
+        message: 'Unable to process request'
+
+      })
+  }
+})
+
+// UPDATE
+
+router.put('/:id', async (req, res) => {
+
+
+})
+
+// DELETE
+
+router.delete('/:id', async (req, res) => {
+
+
+})
 
 module.exports = router;
